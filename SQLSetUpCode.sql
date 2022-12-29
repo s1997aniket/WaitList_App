d51e095b6a71ce640d02779310869c193fb0ced6
s1997aniket <s1997aniket@gmail.com>
2022-12-29 14:42:29 +0530

    Main Code Files

---
+++ SQLSetUpCode.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE join_us;
+
+USE join_us;
+
+CREATE TABLE users (
+	email VARCHAR(255) PRIMARY KEY,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO users (email) 
+VALUES('Katie34@yahoo.com'), ('Tunde@gmail.com');
+
+SELECT * FROM users;
